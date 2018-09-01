0a176b43d53f6050f5e044be69af50d28750093e
NAKAGAKI Kenji <nakaken0629@gmail.com>
2018-09-02 01:16:47 +0900

    initialize ios project

---
+++ ios/OurFingering/Pods/Target Support Files/Pods-OurFingering/Pods-OurFingering-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OurFingering : NSObject
+@end
+@implementation PodsDummy_Pods_OurFingering
+@end
---
+++ ios/OurFingering/Pods/Target Support Files/Pods-OurFingeringTests/Pods-OurFingeringTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OurFingeringTests : NSObject
+@end
+@implementation PodsDummy_Pods_OurFingeringTests
+@end
