35b6efd4ae7217fe801f17b7b24be2ec17a8ab24
NAKAGAKI Kenji <nakaken0629@gmail.com>
2018-09-02 06:36:06 +0900

    Create a main view controller without any logic

--- ios/OurFingering/OurFingering/ViewController.swift
+++ ios/OurFingering/OurFingering/ViewController.swift
@@ -7,19 +7,21 @@
 //
 
 import UIKit
+import Floaty
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let floaty = Floaty()
+        floaty.addItem(title: "from camera")
+        floaty.addItem(title: "from library")
+        self.view.addSubview(floaty)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
